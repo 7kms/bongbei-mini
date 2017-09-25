0d13472229efe832f3116648e430f1544ec27b16
tangliang <tlyspa@gmail.com>
2017-09-25 10:11:47 +0800

    address manager

--- pages/mine/address/index.js
+++ pages/mine/address/index.js
@@ -16,13 +16,12 @@ Page({
             url:'/user/address',
             data: {},
             success: function(data){
-                console.log(data)
                 _this.setData({
                     hiddenLoading: true,
                     list: data.list
                 })
             },
-            fail: function(){
+            fail: function(e){
                 _this.setData({
                     hiddenLoading: true
                 })
@@ -32,7 +31,6 @@ Page({
     setDefault(e){
         let {index} = e.currentTarget.dataset;
         let list = this.data.list
-
         list.forEach((item,i)=>{
             if(index == i){
                 item.isDefault = true
@@ -45,21 +43,56 @@ Page({
         })
         console.log(this.data.list)
     },
-    choseAddress(){
+    remove(e){
+        let {index} = e.currentTarget.dataset;
+        let list = this.data.list;
+        list = list.filter((item,i)=>{
+            return index != i
+        })
+        this.setData({
+            list
+        })
+    },
+    edit(e){
+        let { index } = e.currentTarget.dataset;
+        let list = this.data.list;
+        let isDefault = list[index].isDefault
+        this.choseAddress((address)=>{
+            list[index] = address;
+            list[index].isDefault = isDefault;
+            this.setData({
+                list
+            })
+        })
+    },
+    addAddress(){
+        let list = this.data.list;
+        this.choseAddress((address)=>{
+            list.push(address)
+            this.setData({
+                list
+            })
+        })
+    },
+    choseAddress(callback){
         wx.chooseAddress({
             success(address){
                 delete address.errMsg
+                address.isDefault = false
+                callback && callback(address)
             },
             fail(err){
                 console.log(err)
             }
         })
     },
-    changeRemoteAddress(address){
+    changeRemoteAddress(){
         var _this = this;
         this.setData({
-            lodingText: '提交中'
+            lodingText: '提交中',
+            hiddenLoading: false
         })
+        let address = this.data.list
         $api({
             method:'POST',
             url:'/user/address',
@@ -67,19 +100,20 @@ Page({
                 address
             },
             success: function(data){
-                console.log(data)
                 _this.setData({
-                    hiddenLoading: true,
-                    list: data.list
+                    hiddenLoading: true
                 })
+                wx.showToast({
+                    title: '提交成功',
+                    icon: 'success',
+                    duration: 1500
+                  })
+                setTimeout(wx.navigateBack, 1500);
             },
             fail: function(){
                 _this.setData({
                     hiddenLoading: true
                 })
-            },
-            complete: function(){
-
             }
         })
     }
